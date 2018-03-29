4700e494f84dc69896869505a60018b0cba6efad
Kenjara <kenjara@hotmail.com>
2018-03-29 15:58:25 +0100

    Added sample projectile. Added movement

--- space-game-1.py
+++ space-game-1.py
@@ -6,15 +6,15 @@ import unicornhathd
 import pygame, time
 from pygame.locals import *
 
-shipX = 7
-shipY = 7
+
 
 pygame.init()
+clock = pygame.time.Clock()
 screen = pygame.display.set_mode((300, 10))
 pygame.display.set_caption('Pygame Window')
 pygame.mouse.set_visible(0)
 
-unicornhathd.rotation(0)
+unicornhathd.rotation(90)
 unicornhathd.brightness(1)
 u_width,u_height = unicornhathd.get_shape()
 
@@ -23,27 +23,54 @@ u_width,u_height = unicornhathd.get_shape()
 xPixels = 16
 yPixels = 16
 
+shotX = 0
+shotY = 0
+shotActive = False
+
 def DrawShip(): 
     unicornhathd.set_pixel(shipX, shipY, 255, 255, 0)
+    
+    if(shotActive == True):
+        unicornhathd.set_pixel(shotX, shotY, 0, 0, 255)
+    
     return
 
 try:
+    shipX = 7
+    shipY = 0
     while True:
-        print("loop")
+        #http://qq.readthedocs.io/en/latest/main_loop.html
+        #print("loop")
+        clock.tick(15)
         for event in pygame.event.get():
-            if (event.type == KEYUP):
-                shipY -= 1
-            elif (event.type == KEYDOWN):
-                shipY += 1
+            if (event.type == KEYDOWN):
+                if event.key == pygame.K_LEFT:
+                    shipX -= 1
+                elif event.key == pygame.K_RIGHT:
+                    shipX += 1
+##                elif event.key == pygame.K_UP:
+##                    shipY += 1
+##                elif event.key == pygame.K_DOWN:                        
+##                    shipY -= 1
+                
+                if event.key == pygame.K_SPACE:
+                    shotX = shipX
+                    shotY = shipY + 1
+                    shotActive = True
                 
     
         unicornhathd.clear()
-        print(shipY)
+        #print(shipY)
         DrawShip();
-            
+        
+        if(shotY < yPixels -1):
+            shotY +=1
+        else:
+            shotActive = False
                      
         unicornhathd.show()
                 
 except KeyboardInterrupt:
+    unicornhathd.clear()
     unicornhathd.off()
             
